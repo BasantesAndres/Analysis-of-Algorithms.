da3993bb4706e70b543035d2e550df520d440a4b
Andres <basantes95@hotmail.com>
2026-02-08 21:40:27 -0500

    first commit

---
+++ Iterative.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "How many terms? ";
+    cin >> n;
+
+    long long a = 0, b = 1;
+
+    for (int i = 0; i < n; i++) {
+        cout << a;
+        if (i < n - 1) cout << " ";
+
+        long long c = a + b;
+        a = b;
+        b = c;
+    }
+    cout << endl;
+    return 0;
+}
